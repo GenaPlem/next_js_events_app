62e6cf2584bbe60fb92140e0da003c497b95df07
GenaPlem <shainkill1337@gmail.com>
2024-11-11 13:37:46 +0000

    Add form submit handler function

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,21 +1,34 @@
-import Button from "../ui/button";
+import { useRef } from "react";
 
+import Button from "../ui/button";
 import styles from "./events-search.module.css";
 
 export default function EventsSearch(props) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  };
+
   return (
-    <form action="" className={styles.form}>
+    <form onSubmit={submitHandler} className={styles.form}>
       <div className={styles.controls}>
         <div className={styles.control}>
           <label htmlFor="year">Year</label>
-          <select name="year" id="year">
+          <select name="year" id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
         <div className={styles.control}>
           <label htmlFor="month">Month</label>
-          <select name="month" id="month">
+          <select name="month" id="month" ref={monthInputRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
