1eddf6fc3dd86e2db527e16601fdee3793482d12
GenaPlem <shainkill1337@gmail.com>
2024-11-13 14:23:37 +0000

    Add pre-fetching data to all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
 
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
-export default function AllEventsPage() {
+export default function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const events = props.events;
 
   const findEventsHandler = (year, month) => {
     const fullPath = `/events/${year}/${month}`;
@@ -20,4 +20,15 @@ export default function AllEventsPage() {
       <EventList items={events} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
 }
