cb66eaee29700f148c290f60e0e9f87c69d5685e
GenaPlem <shainkill1337@gmail.com>
2024-11-14 13:07:16 +0000

    Add dynamic meta tags for Event detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -3,6 +3,7 @@ import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import Head from "next/head";
 
 export default function EventDetailPage(props) {
   const event = props.selectedEvent;
@@ -17,6 +18,10 @@ export default function EventDetailPage(props) {
 
   return (
     <>
+      <Head>
+        <title>{event.title}</title>
+        <meta name="description" content={event.description} />
+      </Head>
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
