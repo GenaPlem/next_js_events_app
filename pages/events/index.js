150811590cc00e758adc66adf94311b0723cb36c
GenaPlem <shainkill1337@gmail.com>
2024-11-14 13:06:15 +0000

    Add Head component with title and meta tags to all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
+import Head from "next/head";
 
 export default function AllEventsPage(props) {
   const router = useRouter();
@@ -16,6 +17,13 @@ export default function AllEventsPage(props) {
 
   return (
     <>
+      <Head>
+        <title>All Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
