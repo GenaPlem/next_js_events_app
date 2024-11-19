2d4513e6d58b8dc49f985b3429826ce349fc33ff
GenaPlem <shainkill1337@gmail.com>
2024-11-19 13:13:36 +0000

    Update newsletter handler function with MongoDB

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -7,7 +9,15 @@ export default function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.wgb0k.mongodb.net/newsletter?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    const db = client.db();
+
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
+
     res.status(201).json({ message: "Signed up!" });
   }
 }
