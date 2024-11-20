b6f23189c6e500135bd043032c5c495765d9a976
GenaPlem <shainkill1337@gmail.com>
2024-11-20 11:18:55 +0000

    Add Notification component in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
+
 import Layout from "../components/layout/layout";
+import Notification from "../components/ui/notification";
 
 import "../styles/globals.css";
 
@@ -12,6 +14,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is a test." status="pending" />
     </Layout>
   );
 }
